18c131cde8dda15e31a2d972e10d9b0e5419de24
Ben Briggs <30274540+SGGIRBS@users.noreply.github.com>
2021-08-04 12:32:30 +0100

    Update manual-deploy-tests.ps1

--- manual-deploy-tests.ps1
+++ manual-deploy-tests.ps1
@@ -5,4 +5,4 @@ $parametersFilePath = "./bicep/prod-parameters.json"
 $templateFilePath = "./bicep/main.bicep"
 
 az account set --subscription $subscriptionID
-az deployment group --resource-group $resourceGroupName --confirm-with-what-if --name DW --template-file $templateFilePath --parameters $parametersFilePath
+az deployment group create --confirm-with-what-if --name DW --resource-group $resourceGroupName --template-file $templateFilePath --parameters $parametersFilePath
